3197039c90d8960bd3316c2f32a8719c89d655a4
Megan Koh <mkoh7@gatech.edu>
2018-02-10 15:02:53 -0500

    Making Event Class
    Making Event class

---
+++ app/src/main/java/Event.java
@@ -0,0 +1,50 @@
+import java.io.BufferedInputStream;
+
+/**
+ * Created by Megan on 2/10/18.
+ */
+
+import java.lang.Object;
+
+public enum Attributes {
+    FREEFOOD,
+    FEMALE, MALE,
+    ASIAN, HISPANIC, LATINO, CAUCASIAN, BLACK,
+    LGBTQIA,
+    RELIGIOUS, ACADEMIC, OCCUPATIONAL,
+    GREEK, ARTS, SPORTS, GAMES
+}
+
+public class Event {
+
+    private  pic;
+    String name;
+    int startTime;
+    int endTime;
+    int length;
+    String description;
+    String organization;
+    String location;
+    int latitude;
+    int longitude;
+    Attributes attributes;
+
+    public static void main(String[] args){
+        // attributes
+
+
+
+
+
+
+
+    }
+
+    public void getName(){
+
+    }
+
+    public void setName(){
+
+    }
+}
